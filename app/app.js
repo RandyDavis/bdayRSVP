414fc1cfecbf37a0b70f299c06bc47d10efa93dd
Randy Davis <randy_davis@outlook.com>
2015-05-26 01:09:13 -0500

    set up needed files and initial index.html page;  need to add content to controllers and view templates

---
+++ app/app.js
@@ -0,0 +1,27 @@
+(function() {
+  var app = angular.module('rsvpApp', ['ngRoute', 'ngAnimate']);
+
+  app.config(function($routeProvider) {
+    $routeProvider
+      .when('/', {
+        controller: 'RsvpController',
+        templateUrl: 'index.html'
+      })
+
+      .when('/champ', {
+        controller: 'ChampController',
+        templateUrl: 'app/views/champ.html'
+      })
+
+      .when('/atlas', {
+        controller: 'AtlasController',
+        templateUrl: 'app/views/atlas.html'
+      })
+
+      .when('/rsvp', {
+        controller: 'RsvpController',
+        templateUrl: 'app/views/rsvp.html'
+      })
+      .otherwise({ redirectTo: '/' });
+  });
+}());
